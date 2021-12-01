5279545d8f130e816ae3a9a1f78f24cc9a0f8233
RaghuRaj0038 <80841867+RaghuRaj0038@users.noreply.github.com>
2021-12-01 21:00:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qmAbrECVD/model.json', modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function draw() {
+    Image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidance.toFixed(3);
+    }
+}
+
